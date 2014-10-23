300e5396a839fc72918a1a40edc01fdb8d83b1dd
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-10-23 09:59:47 +0900

    Initial Commit

---
+++ MapEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapEditor
+//
+//  Created by ビザンコムマック０９ on 2014/10/23.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapEditor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MapEditor
+//
+//  Created by ビザンコムマック０９ on 2014/10/23.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
